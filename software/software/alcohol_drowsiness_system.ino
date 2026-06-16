7c11bb4cba9e243c4cd3402a2cdc0fe5cd12282a
AvinashB-310307 <avinashbkkl07@gmail.com>
2026-06-16 12:13:42 +0530

    alcohol_drowsiness_system.ino

--- software/software/alcohol_drowsiness_system.ino
+++ software/software/alcohol_drowsiness_system.ino
@@ -1,1 +1,34 @@
+int alcoholSensor = A0;
+int eyeBlinkSensor = 2;
+int buzzer = 8;
+int relay = 7;
 
+void setup() {
+  pinMode(eyeBlinkSensor, INPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(relay, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  int alcoholValue = analogRead(alcoholSensor);
+  int eyeState = digitalRead(eyeBlinkSensor);
+
+  if(alcoholValue > 400) {
+    digitalWrite(buzzer, HIGH);
+    digitalWrite(relay, LOW);
+  }
+
+  else if(eyeState == LOW) {
+    digitalWrite(buzzer, HIGH);
+  }
+
+  else {
+    digitalWrite(buzzer, LOW);
+    digitalWrite(relay, HIGH);
+  }
+
+  delay(100);
+}
